c5e0d2e927a1d19fe4d0c181ceee8dcea8f89836
CatherineZhuang-cz <lynn_kkcz@berkeley.edu>
2024-10-31 01:38:49 -0700

    Update api/utils.py
    Co-authored-by: Mehul Gandhi <48500458+Mehul-Gandhi@users.noreply.github.com>

--- api/utils.py
+++ api/utils.py
@@ -257,7 +257,7 @@ def extract_assignment_ids(sub_dict: dict):
                 assignment_ids.extend(extract_assignment_ids(value))  # Recursively handle nested dictionaries
     return assignment_ids
 
-def get_ids_for_category(data_dict, category):
+def get_ids_for_category(data_dict: dict, category: str) -> list:
     """
     Get all the assignment id's for one category
 
